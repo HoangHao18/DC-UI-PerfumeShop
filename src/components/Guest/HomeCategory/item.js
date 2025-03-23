a23f6683c7e46b9e0cb482d0bbcfa2327a0cabbe
Hoang Hao <hoanghao198101999@gmail.com>
2025-03-24 00:48:00 +0700

    fix: error src for image

--- src/components/Guest/HomeCategory/item.js
+++ src/components/Guest/HomeCategory/item.js
@@ -13,7 +13,7 @@ export default function Item({
 }){
     return(
         <div className="type-product">
-            <div className="product-image"><img src={image} alt=""></img></div>
+            <div className="product-image"><img src={`${process.env.PUBLIC_URL}${image}`} alt=""></img></div>
             <div className="info">{children}</div>
             <div className="product-name"><Button01 isRadios={isRadios} linkTo={linkTo} width={width} height={height}> {name} </Button01></div>         
         </div>
